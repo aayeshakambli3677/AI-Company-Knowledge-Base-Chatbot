5707047c102382afa6040edfdc480c87588bd2e6
aayeshakambli3677 <aayeshakambli@gmail.com>
2026-08-01 20:15:19 +0530

    lazy load embedding model

--- Backend/app/ai/rag_pipeline.py
+++ Backend/app/ai/rag_pipeline.py
@@ -4,47 +4,46 @@ from app.ai.llm_service import LLMService
 
 
 class RAGPipeline:
-    """
-    Retrieval-Augmented Generation (RAG) Pipeline
-    """
 
     def __init__(self):
-        self.embedding_service = EmbeddingService()
+        self.embedding_service = None
         self.vector_store = VectorStore()
         self.llm_service = LLMService()
 
+    def get_embedding_service(self):
+        if self.embedding_service is None:
+            self.embedding_service = EmbeddingService()
+        return self.embedding_service
+
     def add_documents(
-    self,
-    documents,
-    document_id
-    ):
-        embeddings = self.embedding_service.generate_embeddings(
-        documents
-    )
-        self.vector_store.add_documents(
+        self,
         documents,
-        embeddings,
         document_id
-    )
+    ):
+
+        embeddings = self.get_embedding_service().generate_embeddings(
+            documents
+        )
+
+        self.vector_store.add_documents(
+            documents,
+            embeddings,
+            document_id
+        )
 
     def answer_question(self, question, document_id):
-        """
-        Generate an answer for the user's question.
-        """
 
-        # Create embedding for user question
-        question_embedding = self.embedding_service.generate_embedding(question)
+        question_embedding = (
+            self.get_embedding_service()
+            .generate_embedding(question)
+        )
 
-        # Retrieve relevant documents
         relevant_docs = self.vector_store.search(
             question_embedding,
             top_k=3,
             document_id=document_id
         )
 
-        print("Retrieved Docs:", relevant_docs)
-
-        # Combine retrieved documents
         context = "\n\n".join(relevant_docs)
 
         prompt = f"""
@@ -67,25 +66,3 @@ Answer:
         answer = self.llm_service.generate_response(prompt)
 
         return answer
-
-
-# -----------------------------
-# Example Usage
-# -----------------------------
-if __name__ == "__main__":
-
-    rag = RAGPipeline()
-
-    documents = [
-        "Employees receive 12 casual leaves every year.",
-        "Office timing is 9:30 AM to 6:30 PM.",
-        "Employees must wear ID cards inside the office."
-    ]
-
-    rag.add_documents(documents)
-
-    question = "How many casual leaves are allowed?"
-
-    response = rag.answer_question(question)
-
-    print(response)
